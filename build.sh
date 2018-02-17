2c70fc850173aeba8aa03b6f45a9ba0553883dd4
SP Mohanty <sharada.mohanty@epfl.ch>
2018-02-17 16:54:50 +0100

    working build for osx64

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ echo "Source DIR : ${SRC_DIR}"
 echo "PREFIX : ${PREFIX}"
 echo "RECIPE DIR : ${RECIPE_DIR}"
 
-cp ${SRC_DIR}/bin/xsd ${PREFIX}/bin/
+cp ${SRC_DIR}/bin/xsd ${PREFIX}/bin/xsd-marlo
+cp ${SRC_DIR}/bin/xsd ${PREFIX}/bin/xsd
+
 cp -r ${SRC_DIR}/libxsd/xsd ${PREFIX}/include
